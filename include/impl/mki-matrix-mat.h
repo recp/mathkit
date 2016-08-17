b37928ccf3d458d4a2ddd0ae2562f506b44727c2
Recep Aslantas <m@recp.me>
2016-08-17 15:22:44 +0300

    implement matrix-matix-l mul for other types

--- include/impl/mki-matrix-mat.h
+++ include/impl/mki-matrix-mat.h
@@ -106,6 +106,62 @@ mkMatrixTransposeTo(MkMatrix * __restrict matrix,
 #define mk_mmultl(MK__M, MK__ML, MK__BUFS, MK__LAY)                           \
    mkMatrixMatrixMultL(MK__M, MK__ML, MK__BUFS, MK__LAY)
 
+#define mkMatrixMatrixMultL_impl(T)                                           \
+  do {                                                                        \
+   T     *pL;                                                                 \
+   T     *pR;                                                                 \
+   T     *pD;                                                                 \
+   T      tmpSum;                                                             \
+   size_t rowsL;                                                              \
+   size_t colsR;                                                              \
+   size_t colsL;                                                              \
+   size_t i;                                                                  \
+   size_t j;                                                                  \
+   size_t iR;                                                                 \
+   int    bufindex;                                                           \
+                                                                              \
+   bufindex = matrix->bufindex;                                               \
+                                                                              \
+   pL = (T *)bufs[bufindex];                                                  \
+   pD = (T *)bufs[!bufindex];                                                 \
+   pR = (T *)bufs[2];                                                         \
+                                                                              \
+   /* max allowed size is 4x4 for manual computation */                       \
+   if (mk_builtin_expect(!lay[0].runtime                                      \
+                         && lay[0].count[0] < 5                               \
+                         && lay[0].count[1] < 5                               \
+                         && lay[1].count[0] < 5                               \
+                         && lay[1].count[1] < 5, 1)) {                        \
+                                                                              \
+      mkRawMatrixMult(pL, pR, pD, lay);                                       \
+   } else {                                                                   \
+      if (lay[0].runtime) {                                                   \
+         rowsL = matrixL->rows;                                               \
+         colsL = matrixL->cols;                                               \
+         colsR = matrix->cols;                                                \
+      } else {                                                                \
+         rowsL = lay[0].count[0];                                             \
+         colsL = lay[0].count[1];                                             \
+         colsR = lay[0].count[1];                                             \
+      }                                                                       \
+                                                                              \
+      for (i = 0; i < rowsL; i++)                                             \
+         for (j = 0; j < colsR; j++) {                                        \
+            tmpSum = 0;                                                       \
+            for (iR = 0; iR < colsL; iR ++)                                   \
+               tmpSum += *(pL + i * colsR + iR) * *(pR + iR * colsR + j);     \
+                                                                              \
+            *pD++ = tmpSum;                                                   \
+         }                                                                    \
+   }                                                                          \
+                                                                              \
+   matrix->bufindex = !bufindex;                                              \
+   matrix->value    = bufs[!bufindex];                                        \
+                                                                              \
+   if (bufs[bufindex] == bufs[2])                                             \
+      bufs[2] = bufs[!bufindex];                                              \
+  } while (0)
+
 MK_INLINE
 void
 mkMatrixMatrixMultL(MkMatrix * __restrict matrix,
@@ -113,66 +169,10 @@ mkMatrixMatrixMultL(MkMatrix * __restrict matrix,
                     void * __restrict bufs[3],
                     const MkBufLayout lay[2]) {
    switch (lay[0].type) {
-      case MK_FLOAT: {
-         float  *posA;
-         float  *posB;
-         float  *posC;
-         float  tmpSum;
-         size_t rowsL;
-         size_t colsR;
-         size_t colsL;
-         size_t i;
-         size_t j;
-         size_t iR;
-         int    bufindex;
-
-         bufindex = matrix->bufindex;
-
-         posA = (float *)bufs[bufindex];
-         posC = (float *)bufs[!bufindex];
-         posB = (float *)bufs[2];
-
-         /* max allowed size is 4x4 for manual computation */
-         if (mk_builtin_expect(!lay[0].runtime
-                               && lay[0].count[0] < 5
-                               && lay[0].count[1] < 5
-                               && lay[1].count[0] < 5
-                               && lay[1].count[1] < 5, 1)) {
-
-            mkRawMatrixMult((float *)posA,
-                            (float *)posB,
-                            (float *)posC,
-                            lay);
-         } else {
-            if (lay[0].runtime) {
-               rowsL = matrixL->rows;
-               colsL = matrixL->cols;
-               colsR = matrix->cols;
-            } else {
-               rowsL = lay[0].count[0];
-               colsL = lay[0].count[1];
-               colsR = lay[0].count[1];
-            }
-
-            for (i = 0; i < rowsL; i++)
-               for (j = 0; j < colsR; j++) {
-                  tmpSum = 0.0f;
-                  for (iR = 0; iR < colsL; iR ++)
-                     tmpSum += *(posA + i * colsR + iR)
-                                   * *(posB + iR * colsR + j);
-
-                  *posC++ = tmpSum;
-               }
-         }
-
-         matrix->bufindex = !bufindex;
-         matrix->value    = bufs[!bufindex];
-
-         if (bufs[bufindex] == bufs[2])
-            bufs[2] = bufs[!bufindex];
-
-         break;
-      }
+      case MK_FLOAT:  mkMatrixMatrixMultL_impl(float);   break;
+      case MK_DOUBLE: mkMatrixMatrixMultL_impl(double);  break;
+      case MK_INT32:  mkMatrixMatrixMultL_impl(int32_t); break;
+      case MK_INT64:  mkMatrixMatrixMultL_impl(int64_t); break;
    }
 }
 
